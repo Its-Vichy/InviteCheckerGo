2af81653ca6be05783047ac18ae763f28f1a1ef6
&! Ѵιchy.#0110 <Its_Vichy@protonmail.com>
2022-05-31 04:01:24 +0200

    🧙‍♂️ New features !

--- main.go
+++ main.go
@@ -27,6 +27,9 @@ var (
 	checked     int
 	finished    bool
 	not_enought int
+	blacklisted int
+	higthlevel  int
+	blacklist   []string
 )
 
 type InviteCode struct {
@@ -86,7 +89,7 @@ func readLines(path string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -97,14 +100,24 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+func Include(item string, list []string) bool {
+	for _, value := range list {
+		if value == item {
+			return true
+		}
+	}
+
+	return false
+}
+
 func checkInvite(invite string, proxy string, config Config) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://canary.discord.com/api/v6/invite/%s?with_counts=true", invite), nil)
 	if err != nil {
-		fmt.Printf("[ERROR] %s", err)
+		//fmt.Printf("[ERROR] %s", err)
 		errors++
 		return
 	}
-	
+
 	url_i := url.URL{}
 	url_proxy, _ := url_i.Parse(proxy)
 
@@ -112,10 +125,10 @@ func checkInvite(invite string, proxy string, config Config) {
 	transport.Proxy = http.ProxyURL(url_proxy)
 	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	client := http.Client{Transport: &transport}
-	
+
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("[ERROR] %s", err)
+		//fmt.Printf("[ERROR] %s", err)
 		errors++
 		return
 	}
@@ -123,27 +136,58 @@ func checkInvite(invite string, proxy string, config Config) {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("[ERROR] %s", err)
+		//fmt.Printf("[ERROR] %s", err)
 		errors++
 		return
 	}
-	
+
+	cfile, err := os.OpenFile("checked.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		//log.Fatal(err)
+		errors++
+	}
+
+	_, err = cfile.WriteString(fmt.Sprintf("%s\n", invite))
+	if err != nil {
+		//log.Fatal(err)
+		errors++
+	} else {
+		valid++
+	}
+
+	cfile.Close()
+
 	var code = InviteCode{}
 	err = json.Unmarshal(body, &code)
 	if err != nil {
-		fmt.Printf("Invalid invite: %s\n", invite)
+		//fmt.Printf("Invalid invite: %s\n", invite)
 		invalid++
 		return
 	}
-	
+
+	if Include(code.Guild.ID, blacklist) {
+		//fmt.Printf("Blacklisted invite: %s\n", invite)
+		blacklisted++
+		return
+	}
+
+	// already checked uwu
+	blacklist = append(blacklist, code.Guild.ID)
+
+	// check if need to get phone verfied with the verification level
+	if code.Guild.VerificationLevel == 4 {
+		higthlevel++
+		return
+	}
+
 	percentage := float64(code.ApproximatePresenceCount) / float64(code.ApproximateMemberCount) * 100
 	if code.ApproximateMemberCount < config.MinMembers {
-		fmt.Printf("%s - Not enough members\n", code.Code)
+		//fmt.Printf("%s - Not enough members\n", code.Code)
 		not_enought++
 		return
 	}
 	if percentage < float64(config.MinPercentage) {
-		fmt.Printf("%s - Not enough online members\n", code.Code)
+		//fmt.Printf("%s - Not enough online members\n", code.Code)
 		not_enought++
 		return
 	}
@@ -151,13 +195,13 @@ func checkInvite(invite string, proxy string, config Config) {
 	fmt.Printf("%s - Online: %d/%d (%.2f%%)\n", code.Code, code.ApproximatePresenceCount, code.ApproximateMemberCount, percentage)
 	file, err := os.OpenFile("code.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 		errors++
 	}
-	
+
 	_, err = file.WriteString(fmt.Sprintf("%s:%s\n", code.Code, code.Guild.ID))
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 		errors++
 	} else {
 		valid++
@@ -171,7 +215,7 @@ func loadConfig() Config {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	var config = Config{}
 	scanner := bufio.NewScanner(file)
 
@@ -195,12 +239,28 @@ func include_codes(invite string, list []string, element int) bool {
 	return false
 }
 
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		item = strings.ToLower(item) // lel lowercase
+
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
 func main() {
 	config := loadConfig()
 
 	code, err := readLines("code.txt")
 	lines, err := readLines("invites.txt")
 	proxies, err := readLines(config.ProxiesPath)
+	blacklist, err = readLines("blacklist.txt")
+	checked_codes, err := readLines("checked.txt")
 
 	if err != nil {
 		panic(err)
@@ -213,18 +273,35 @@ func main() {
 			}
 
 			time.Sleep(time.Second * 1)
-			changeTerminalName(fmt.Sprintf("Checked: %d/%d - Invalid: %d - Error: %d - Valid: %d - NotEnought: %d", checked, len(lines), invalid, errors, valid, not_enought))
+			changeTerminalName(fmt.Sprintf("Checked: %d/%d - Invalid: %d - Error: %d - Valid: %d - NotEnought: %d - Blacklisted: %d VerificationLevel: %d", checked, len(lines), invalid, errors, valid, not_enought, blacklisted, higthlevel))
 		}
+
+		fmt.Printf("\n\nChecked: %d/%d - Invalid: %d - Error: %d - Valid: %d - NotEnought: %d - Blacklisted: %d VerificationLevel: %d", checked, len(lines), invalid, errors, valid, not_enought, blacklisted, higthlevel)
 	}()
+
+	// blacklist already checked guild
+	for _, invite := range code {
+		blacklist = append(blacklist, strings.Split(invite, ":")[1])
+	}
+
+	// blacklist already checked invites
+	for _, invite := range checked_codes {
+		code = append(code, strings.Split(invite, ":")[0])
+	}
 
 	c := goccm.New(config.Threads)
 	for _, invite := range lines {
 		c.Wait()
 		go func(invite string) {
+			if strings.Contains(invite, ".gg/") {
+				invite = strings.Split(invite, ".gg/")[1]
+			}
+
 			if !include_codes(invite, code, 0) {
 				checkInvite(invite, config.ProxiesType+"://"+proxies[rand.Intn(len(proxies))], config)
 			} else {
-				fmt.Printf("Duplicate invite: %s\n", invite)
+				//fmt.Printf("Duplicate invite: %s\n", invite)
+				blacklisted++
 			}
 			checked++
 			c.Done()
